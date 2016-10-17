5aba8ef7fee1128ff0233307137ab6808786dff0
Jhantelle Belleza <jhantellebelleza@Jhantelles-MacBook-Pro.local>
2016-10-17 11:00:21 -0400

    Completed Exercise

--- YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
+++ YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
@@ -10,15 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var shrinkConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+    let duration = 1.5
+    let delay = 0.25
+    let options = UIViewKeyframeAnimationOptions.beginFromCurrentState
         
-
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    
+    UIView.animateKeyframes(withDuration: duration, delay: delay, options: options, animations: {
+        UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.1, animations: {
+            self.shrinkConstraint.isActive = true
+            self.view.layoutIfNeeded()})
+        UIView.addKeyframe(withRelativeStartTime: 0.2, relativeDuration: 0.0, animations: {
+            self.shrinkConstraint.isActive = false
+            self.heightConstraint.isActive = true
+            self.view.layoutIfNeeded()})
+        UIView.addKeyframe(withRelativeStartTime: 1.75, relativeDuration: 0.34, animations: {
+            self.heightConstraint.isActive = false
+            self.view.layoutIfNeeded()
+             })
+        }
+            , completion: nil)
     }
-
 }
 
